00ee3917017f1fbfc81fb2260bbec74f91264b46
Shai <shaibeer@gmail.com>
2017-08-03 12:08:11 +0300

    // RN 47.0 deprectated

--- android/src/main/java/nl/springtree/reactnativeamplitudesdk/AmplitudeSDKAndroidPackage.java
+++ android/src/main/java/nl/springtree/reactnativeamplitudesdk/AmplitudeSDKAndroidPackage.java
@@ -27,7 +27,7 @@ public class AmplitudeSDKAndroidPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // RN 47.0 deprectated
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
